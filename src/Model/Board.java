0bd138ece7748296b042ceef1ba66522e9b02604
seguanzon <seguanzon@gmail.com>
2021-10-19 10:09:48 -0700

    added empty methods to Board class

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -2,4 +2,25 @@ package Model;
 
 public class Board {
 
+    int[][]boardData = new int[4][4];
+
+    public void populateTiles() {
+
+    }
+
+    public void changeTheme() {
+
+    }
+
+    public int[][] getBoardData() {
+        // will change this later
+        return boardData;
+    }
+
+    public int[][] updateBoardData() {
+        // will also change this later
+        return boardData;
+    }
+
+
 }
