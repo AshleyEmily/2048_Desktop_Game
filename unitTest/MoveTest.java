9587c7fb80e3ca4c8e6e67207c62e6f8e878ee9f
Atsuya Yano <60360870+atsuyay@users.noreply.github.com>
2021-11-04 21:15:56 -0700

    Accepts key input

---
+++ unitTest/MoveTest.java
@@ -0,0 +1,13 @@
+import Model.Move;
+import org.junit.Test;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MoveTest {
+    public static void main(String[] args) {
+        Move mv = new Move();
+        mv.setVisible(true);
+    }
+
+}
