a190bd63114709e114a786cb8bea508b6abdef57
Atsuya Yano <60360870+atsuyay@users.noreply.github.com>
2021-11-05 23:25:27 -0700

    Improved test cases

--- src/Model/Move.java
+++ src/Model/Move.java
@@ -22,19 +22,15 @@ public class Move extends JFrame implements KeyListener {
         int keyCode = e.getKeyCode();
         switch (keyCode){
             case KeyEvent.VK_UP:
-               // System.out.println("up");
                 break;
 
             case KeyEvent.VK_DOWN:
-               // System.out.println("down");
                 break;
 
             case KeyEvent.VK_RIGHT:
-                //System.out.println("right");
                 break;
 
             case KeyEvent.VK_LEFT:
-                //System.out.println("left");
                 break;
 
             default:
