7094be545df26314a32773d572547614cb4f6543
AshleyEmily <ashleyemily2nd@gmail.com>
2021-11-16 16:14:24 +0800

    Update Controller.java

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,3 +1,35 @@
 package Controller;
 
+import Model.Move;
 
+public class controller {
+    public controller(){
+
+    }
+
+    public void Move(){
+        Move move = new Move();
+        move.setVisible(true);
+    }
+
+    public void startGame(){
+
+    }
+
+    public void changeSetting(){
+
+    }
+
+    public void manageScore(){
+
+    }
+
+    public void restartGame(){
+
+    }
+
+    public void displayBoard(){
+
+    }
+
+}
