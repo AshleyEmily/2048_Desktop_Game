673c42485c31e5352d91bb054e98a688169d499e
AshleyEmily <ashleyemily2nd@gmail.com>
2021-11-21 16:34:44 +0800

    deleted Move class and updated Model class to implement KeyListener

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,12 +1,63 @@
 package Model;
 
-public class Model{
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+
+public class Model extends JFrame implements KeyListener {
 	
 	Board game;
 	
 	Model(){
 		game = new Board();
+        addKeyListener(this);
 	}
+	
+	 @Override
+	    public void keyTyped(KeyEvent e) {
+	        // invalid
+	    }
+
+	    /**
+	     * @Override
+	     * input key action and reflect based on the input.
+	     * */
+	    public void keyPressed(KeyEvent e) {
+	        int keyCode = e.getKeyCode();
+
+	        switch (keyCode){
+	            case KeyEvent.VK_UP:
+	            	game.moveUp();
+	            	//call board move methods moveUp()
+
+	                break;
+
+	            case KeyEvent.VK_DOWN:
+	            	game.moveDown();
+	                break;
+
+	            case KeyEvent.VK_RIGHT:
+	            	game.moveRight();
+	                break;
+
+	            case KeyEvent.VK_LEFT:
+	            	game.moveLeft();
+	                break;
+
+	            default:
+	                break;
+	        }
+	    }
+
+	    @Override
+	    public void keyReleased(KeyEvent e) {
+
+	    }
+
+	    public void setVisible(boolean b) {
+	    }
+
 	
 
 }
