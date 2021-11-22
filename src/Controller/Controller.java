2b772bd1908b9a184c39707d8fcd93545f269c84
atsuyay <60360870+atsuyay@users.noreply.github.com>
2021-11-21 18:50:57 -0800

    Controller

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -4,18 +4,26 @@ package Controller;
 import Model.Model;
 import View.View;
 
+import java.awt.event.ActionEvent;
 
-public class controller {
+
+public class Controller {
     private Model model;
     private View view;
 
     public static void main(String[] args) {
-        new controller();
+        new Controller();
     }
 
-    public controller(){
+    public Controller(){
         model = new Model();
         view = new View();
         view.display();
+    }
+
+    public actionPerformed(ActionEvent e){
+        model.getHighScore();
+        model.getTheme();
+
     }
 }
