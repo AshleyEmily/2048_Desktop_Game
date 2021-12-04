8b638b4cf4a232ca869c5c5cd7639c6e32a61b43
seguanzon <seguanzon@gmail.com>
2021-12-04 10:51:43 -0800

    added javadoc comments for View pckg and Cell

--- src/View/ControlPanel.java
+++ src/View/ControlPanel.java
@@ -13,18 +13,32 @@ import java.awt.*;
  *
  */
 
+/**
+ * ControlPanel constructs the display of buttons: START GAME, CLASSIC THEME, SKY THEME, FOREST THEME
+ */
 public class ControlPanel {
     private static final Insets regularInsets = new Insets(10, 10, 0, 10); //sets padding
     private GameFrame frame;
     private Model model;
     private JPanel panel;
 
+    /**
+     * @param frame creates an object of GameFrame
+     * @param model creates an object of Model
+     * Calls createPartControl() to create new objects of the buttons' action listeners
+     */
     public ControlPanel(GameFrame frame, Model model) {
         this.frame = frame;
         this.model = model;
         createPartControl();
     }
 
+    /**
+     * Creates the four JButtons: startGameButton, theme1Button, theme2Button, and theme3Button
+     * Adds respective action listeners to these JButtons
+     * Creates the JPanel where these buttons will be
+     * Calls to addComponent() method which further formats the JPanel of JButtons
+     */
     private void createPartControl() {
         StartGameActionListener listener = new StartGameActionListener(frame, model);
         Theme1Listener theme1Listener = new Theme1Listener(frame, model);
@@ -34,13 +48,12 @@ public class ControlPanel {
         panel = new JPanel();
         panel.setLayout(new GridBagLayout());
 
-        int gridY = 0;
+        int gridY = 0;  //for formatting y-coord of the buttons in the panel
 
         JButton startGameButton = new JButton("START GAME");
         startGameButton.addActionListener(listener);
         addComponent(panel, startGameButton, 0, gridY++, 1, 1, regularInsets, GridBagConstraints.LINE_START, GridBagConstraints.HORIZONTAL);
 
-        // need to figure out how to make the listener do a popup
         JButton theme1Button = new JButton("CLASSIC THEME");
         theme1Button.addActionListener(theme1Listener);
         addComponent(panel, theme1Button, 0, gridY++, 1, 1, regularInsets, GridBagConstraints.LINE_START, GridBagConstraints.HORIZONTAL);
@@ -54,12 +67,27 @@ public class ControlPanel {
         addComponent(panel, theme3Button, 0, gridY++, 1, 1, regularInsets, GridBagConstraints.LINE_START, GridBagConstraints.HORIZONTAL);
     }
 
-
+    /**
+     *
+     * @param container the container which holds the buttons, ie the JPanel
+     * @param component the component which is being added to the container, ie the JButton
+     * @param gridX x-coordinate where the button is being placed
+     * @param gridY y-coordinate where the button is being placed
+     * @param gridWidth width of the panel
+     * @param gridHeight height of the panel
+     * @param insets borders surrounding the buttons
+     * @param anchor determines the position of the buttons within the panel, ie LINE_START makes them flush to the left
+     * @param fill determines the resizing of the buttons within the panel, ie HORIZONTAL sets the buttons to 100% the width of the panel
+     */
     private void addComponent(Container container, Component component, int gridX, int gridY, int gridWidth, int gridHeight,
             Insets insets, int anchor, int fill) {
         GridBagConstraints gbc = new GridBagConstraints(gridX, gridY, gridWidth, gridHeight, 1, 1, anchor, fill, insets, 0, 0);
         container.add(component, gbc);
     }
 
+    /**
+     *
+     * @return JPanel containing the four buttons
+     */
     public JPanel getPanel() {return panel;}
 }
--- src/View/GameFrame.java
+++ src/View/GameFrame.java
@@ -15,6 +15,11 @@ import java.awt.event.WindowEvent;
  * @author Samantha Guanzon
  *
  */
+
+/**
+ * GameFrame constructs the display of the entire game window
+ * includes: ControlPanel, GridPanel, and ScorePanel
+ */
 public class GameFrame extends JPanel {
 
     private ControlPanel controlPanel;
@@ -23,11 +28,20 @@ public class GameFrame extends JPanel {
     private JFrame frame;
     private ScorePanel scorePanel;
 
+    /**
+     * GameFrame Constructor makes new object of Model
+     * Calls createPartControl() to create new objects of the three other panels
+     * @param model
+     */
     public GameFrame(Model model) {
         this.model = model;
         createPartControl();
     }
 
+    /**
+     * Creates new objects of: GridPanel, ScorePanel, and ControlPanel
+     * Creates JFrame containing all 3 panels
+     */
     private void createPartControl() {
         gridPanel = new GridPanel(model);
         scorePanel = new ScorePanel(model);
@@ -37,8 +51,7 @@ public class GameFrame extends JPanel {
         frame.setTitle("2048");
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent event) {
+            @Override public void windowClosing(WindowEvent event) {
                 exitProcedure();
             }
         });
@@ -57,6 +70,12 @@ public class GameFrame extends JPanel {
         frame.setVisible(true);
     }
 
+    /**
+     * Creates the left sidebar
+     * Adds scorePanel and controlPanel to the left sidebar
+     * This also implies that gridPanel will be added to the right
+     * @return sidePanel which contains the
+     */
     private JPanel createSidePanel() {
         JPanel sidePanel = new JPanel();
         sidePanel.setLayout(new BoxLayout(sidePanel,
@@ -67,6 +86,10 @@ public class GameFrame extends JPanel {
         return sidePanel;
     }
 
+    /**
+     * Creates key bindings for each arrows
+     * Allows the user to operate the game using the WASD keys or the arrow keys on their keyboard
+     */
     private void setKeyBindings() {
         InputMap inputMap =
                 gridPanel.getInputMap(JPanel.WHEN_IN_FOCUSED_WINDOW);
@@ -80,13 +103,6 @@ public class GameFrame extends JPanel {
         inputMap.put(KeyStroke.getKeyStroke("LEFT"), "left arrow");
         inputMap.put(KeyStroke.getKeyStroke("RIGHT"), "right arrow");
 
-        inputMap = gridPanel.getInputMap(JPanel.WHEN_FOCUSED);
-        inputMap.put(KeyStroke.getKeyStroke("UP"), "up arrow");
-        inputMap.put(KeyStroke.getKeyStroke("DOWN"), "down arrow");
-        inputMap.put(KeyStroke.getKeyStroke("LEFT"), "left arrow");
-        inputMap.put(KeyStroke.getKeyStroke("RIGHT"), "right arrow");
-
-
         gridPanel.getActionMap().put("up arrow",
                 new UpAction(this, model));
         gridPanel.getActionMap().put("down arrow",
@@ -97,16 +113,27 @@ public class GameFrame extends JPanel {
                 new RightAction(this, model));
     }
 
+    /**
+     * saves the game attributes by calling saveGame()
+     * clears JFrame and exits
+     */
     public void exitProcedure() {
     	model.saveGame();
         frame.dispose();
         System.exit(0);
     }
 
+    /**
+     * repaints gridPanel -- udpates the view
+     */
     public void repaintGridPanel() {
         gridPanel.repaint();
     }
 
+    /**
+     * updates the score in scorePanel
+     * calls updatePartControl() in ScorePanel class
+     */
     public void updateScorePanel() {
         scorePanel.updatePartControl();
     }
